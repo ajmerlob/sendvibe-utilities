c26f6b919bc85cab5ba9b0b86c725fb4e7c2bc9c
Ubuntu <ubuntu@ip-172-31-6-198.us-west-2.compute.internal>
2017-11-29 04:54:56 +0000

    added scrub to util class

--- util.py
+++ util.py
@@ -12,3 +12,8 @@ class Util:
     self.s.login(sender, password)
     self.s.sendmail(sender,recipient,message)
     self.s.quit()
+
+  def scrub(self,txt):
+    if "<" in txt and ">" in txt:
+      return txt.split('<')[1].split(">")[0]
+    return txt
