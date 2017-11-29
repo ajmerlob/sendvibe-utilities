08b090cd98741793aa3e9bcaaf103d775c023a73
Ubuntu <ubuntu@ip-172-31-6-198.us-west-2.compute.internal>
2017-11-29 05:24:03 +0000

    updated scrub logic

--- util.py
+++ util.py
@@ -1,6 +1,13 @@
 import math
 import sys
 import smtplib
+import re
+result = re.search('asdf=5;(.*)123jasd', s)
+print result.group(1)import re
+
+s = 'asdf=5;iwantthis123jasd'
+result = re.search('asdf=5;(.*)123jasd', s)
+print result.group(1)
 
 class Util:
   def __init__(self):
@@ -14,6 +21,24 @@ class Util:
     self.s.quit()
 
   def scrub(self,txt):
+    def _scrub(pattern,txt):
+      try:
+        email = re.search(pattern,txt).group(1)
+        if '@' in email:
+          return email
+        else:
+          email2 = email.replace(email,"").strip()
+          if '@' in email2
+            return email2
+          else:
+            return txt
+      except:
+        return txt
+ 
     if "<" in txt and ">" in txt:
-      return txt.split('<')[1].split(">")[0]
+      return _scrub("<(.*)>",txt)
+    elif "(" in txt and ")" in txt:
+      return _scrub("\((.*)\)",txt)
+    elif '"' in txt:
+      return _scrub('"(.*)"',txt)
     return txt
